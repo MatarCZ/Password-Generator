4df9afe032760abb629a72917e75917f68349079
MatarCZ <marek.furis@seznam.cz>
2025-06-18 20:15:21 +0200

    Added simple input handling

--- src/pass.cpp
+++ src/pass.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <time.h>
+#include <string>
+
+using namespace std;
 
 int main() {
     srand(time(0)); // Seed the random number generator
-
+    string phrase;
+    cout << "Enter phrase for password generation: ";
+    cin >> phrase;
+    return 0;
 }
