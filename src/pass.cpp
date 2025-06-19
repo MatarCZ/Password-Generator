0ea074f07a36ec1dc20e7f96e372fd136c5af2a5
MatarCZ <marek.furis@seznam.cz>
2025-06-19 08:46:07 +0200

    Added input handling (Letters only) with conversion to uppercase.

--- src/pass.cpp
+++ src/pass.cpp
@@ -11,12 +11,31 @@ int main() {
     
     string phrase = userInput(); // Get user input for the password generation
 
+    char map[7][3] = {
+        {'q', 'a', 'z'},
+        {'w', 's', 'x'},
+        {'e', 'd', 'c'},
+        {'r', 'f', 'v'},
+        {'t', 'g', 'b'},
+        {'y', 'h', 'n'},
+        {'u', 'j', 'm'}
+    };
+
     return 0; // End of the program
 }
 
 string userInput() {
     string phrase;
-    cout << "Enter phrase for password generation: ";
+    cout << "Enter phrase for password generation (letters only): ";
     cin >> phrase;
-    return phrase; // Return the user input
+
+    // Validate input and convert to uppercase
+    for (size_t i = 0; i < phrase.size(); ++i) {
+        if (!isalpha(phrase[i])) {
+            cout << "Invalid input. Please enter letters only." << endl;
+            return userInput(); // Recursively ask for valid input
+        }
+        phrase[i] = toupper(phrase[i]); // Convert to uppercase
+    }
+    return phrase; // Return the validated and converted input
 }
