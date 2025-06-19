cb83f305536c781a4e3a339b395689a8d4d7c8e4
mortumvitae <mira.kocirik@seznam.cz>
2025-06-19 09:57:44 +0200

    Changed cout

--- src/pass.cpp
+++ src/pass.cpp
@@ -49,7 +49,7 @@ int main() {
 
 string userInput() {
     string phrase;
-    cout << "Enter phrase for password generation (letters only): ";
+    cout << "Enter phrase for password generation (letters only, no spaces): ";
     cin >> phrase;
 
     // Validate input and convert to uppercase
